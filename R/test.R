d82b31a1fa11a283db8b26a554dabb2522d32e6e
Arne_Hegel <arne.stuff@gmx.de>
2020-07-06 15:33:39 +0200

    testing data.tree

--- R/test.R
+++ R/test.R
@@ -1,1 +1,11 @@
-foo <- function(x,y) x+y
+L <- function(y, s){
+  return((y-s)>1e-5)
+}
+
+createTree <- function(X, Y, depth){
+  T <- data.tree::Node$new("0", s = mean(X), depth = 0)
+  for(i in 1:depth){
+    data.tree::T$AddChild(sprintf("%d", i), s = mean(X), depth = i)
+
+  }
+}
