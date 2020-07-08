791422a5567529a12d3b603d3b1f498381161fb6
Arne_Hegel <arne.stuff@gmx.de>
2020-07-08 18:18:12 +0200

    testing s4 trees

--- R/test.R
+++ R/test.R
@@ -9,3 +9,17 @@ createTree <- function(X, Y, depth){
 
   }
 }
+
+.Tree <- setClass("Tree",
+                    slots = c(childs = "list", depth = "numeric", s = "numeric", y = "numeric")
+)
+.BinaryTree <- setClass("BinaryTree",
+                  contains = "Tree",
+                  slots = c(parent = "list")
+)
+a <- .Tree(childs = list(b1, b2), depth = 0, s = 0.5)
+b1 <- .Tree(childs = list(c1), depth = 1, s = 0.25)
+b2 <- .Tree(childs = list(c2), depth = 1, s = 0.25)
+c1 <- .Tree(childs = list(), depth = 2, y = )
+c2 <- .Tree(childs = list(), depth = 2)
+
