a449a205d21555f753d345cd768a31dd9a5ed142
Arne Hegel <arnehegel@gmx.de>
2020-07-13 16:46:39 +0200

    added get_s(), get_y() and Tree$print()
    tested by binary regression tree (Beispiel 6.3)

--- R/test.R
+++ R/test.R
@@ -32,6 +32,7 @@ library(R6)
 Tree <- R6Class("Tree", list(
   name = NULL,
   root = NA,
+  sub_root = NA,
   initialize = function(name, root) {
     stopifnot(is.character(name), length(name) == 1)
 
@@ -43,6 +44,16 @@ Tree <- R6Class("Tree", list(
     cat("Baum: \n")
     cat("  Name: ", self$name, "\n", sep = "")
     cat("  Tiefe:  ", self$get_depth(), "\n", sep = "")
+    cat("  S:  ", self$get_s(), "\n", sep = "")
+    cat("  Y:  ", self$get_y(), "\n", sep = "")
+
+    x <- c(0, self$get_s(), 1)
+    for (i in 1:(length(x)-1)) {
+      x[i] <- mean(x[i:(i+1)])
+    }
+    x <- x[1:(length(x)-1)]
+    y <- self$get_y()
+    plot(x,y)
     invisible(self)
   },
 
@@ -56,22 +67,33 @@ Tree <- R6Class("Tree", list(
 
   get_depth = function(){
     return(self$get_root()$get_depth())
+  },
+
+  get_s = function(){
+    return(self$get_root()$get_s())
+  },
+
+  get_y = function(){
+    return(self$get_root()$get_y())
   }
 ))
 
 Node <- R6Class("Node", list(
-  s = 0,
+  s = NULL,
+  y = NULL,
   left_child = 0,
   right_child = 0,
-  initialize = function(s = 0) {
-    self$s <- s
+  initialize = function() {
+
   },
 
   print = function(...) {
     cat("Knoten: \n")
     cat("  s: ", self$s, "\n", sep = "")
+    cat("  y: ", self$y, "\n", sep = "")
     cat("  Teilbaumtiefe:  ", self$get_depth(), "\n", sep = "")
     cat("  Blatt:  ", self$is_leef(), "\n", sep = "")
+
     invisible(self)
   },
 
@@ -104,16 +126,42 @@ Node <- R6Class("Node", list(
     return(max(self$left_child$get_depth() + 1, self$right_child$get_depth() + 1))
   },
 
+  get_s = function(){
+    if(class(self$left_child) != "Node" && class(self$right_child) != "Node"){
+      return(self$s)
+    }
+    if(class(self$left_child) == "Node" && class(self$right_child) != "Node"){
+      return(c(self$left_child$get_s(), self$s))
+    }
+    if(class(self$left_child) != "Node" && class(self$right_child) == "Node"){
+      return(c(self$s ,self$right_child$get_s()))
+    }
+    return( c(self$left_child$get_s(), self$s, self$right_child$get_s()) )
+  },
+
+  get_y = function(){
+    if(class(self$left_child) != "Node" && class(self$right_child) != "Node"){
+      return(self$y)
+    }
+    if(class(self$left_child) == "Node" && class(self$right_child) != "Node"){
+      return(c(self$left_child$get_y(), self$y))
+    }
+    if(class(self$left_child) != "Node" && class(self$right_child) == "Node"){
+      return(c(self$y ,self$right_child$get_y()))
+    }
+    return( c(self$left_child$get_y(), self$right_child$get_y()) )
+  },
+
   is_leef = function(){
     return( self$get_depth() == 1 )
   }
 ))
 
 a <- Node$new()
-a$add_left_child(Node$new(1))
-a$add_right_child(Node$new(2))
-a$left_child$add_left_child(Node$new(3))
-a$left_child$add_right_child(Node$new(4))
+a$add_left_child(Node$new())
+a$add_right_child(Node$new())
+a$left_child$add_left_child(Node$new())
+a$left_child$add_right_child(Node$new())
 b_tree1 <- Tree$new("binary tree 1", a)
 b_tree1
 a
@@ -134,3 +182,33 @@ b_tree2
 b
 
 
+#---------------------------------------
+
+
+# simulate some data
+set.seed(0)
+n <- 200
+x <- runif(n)
+y <- 1 + 2*x + rnorm(n, mean = 0, sd = 0.5)
+y <- sin(2*pi*x) + rnorm(n, mean = 0, sd = 0.2**2)
+plot(x,y)
+
+create_regression_tree <- function(node, x, y, depth){
+  if(depth > 0){
+    s = mean(x)
+    node$s = s
+    node$add_left_child(Node$new())
+    node$add_right_child(Node$new())
+    create_regression_tree(node$get_left_child(), x[x<s], y[x<s], depth - 1)
+    create_regression_tree(node$get_right_child(), x[x>=s], y[x>=s], depth - 1)
+  }
+  else{
+    node$y = mean(y)
+  }
+}
+
+b <- Node$new()
+create_regression_tree(b, x, y, 3)
+b_regression_tree <- Tree$new("binary regression tree", b)
+b_regression_tree
+b
