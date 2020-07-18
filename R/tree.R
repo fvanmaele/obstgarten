5315eb91be3bd7f031484e93f602b0a6c5190dff
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2020-07-18 18:33:07 +0200

    move Tree class to R/tree.R and test-tree.R

--- R/tree.R
+++ R/tree.R
@@ -1,9 +1,3 @@
-#---------------------------------------
-#R6 test
-
-# https://adv-r.hadley.nz/r6.html
-library(R6)
-
 Tree <- R6Class("Tree", list(
   name = NULL,
   root = NA,
@@ -132,58 +126,3 @@ Node <- R6Class("Node", list(
   }
 ))
 
-a <- Node$new()
-a$add_left_child(Node$new())
-a$add_right_child(Node$new())
-a$left_child$add_left_child(Node$new())
-a$left_child$add_right_child(Node$new())
-b_tree1 <- Tree$new("binary tree 1", a)
-b_tree1
-a
-a$left_child$left_child
-
-create_tree <- function(node, depth){
-  if(depth > 0){
-    node$add_left_child(Node$new())
-    node$add_right_child(Node$new())
-    create_tree(node$get_left_child(), depth - 1)
-    create_tree(node$get_right_child(), depth - 1)
-  }
-}
-b <- Node$new()
-create_tree(b, 10)
-b_tree2 <- Tree$new("binary tree 2", b)
-b_tree2
-b
-
-
-#---------------------------------------
-
-
-# simulate some data
-set.seed(0)
-n <- 200
-x <- runif(n)
-y <- 1 + 2*x + rnorm(n, mean = 0, sd = 0.5)
-y <- sin(2*pi*x) + rnorm(n, mean = 0, sd = 0.2**2)
-plot(x,y)
-
-create_regression_tree <- function(node, x, y, depth){
-  if(depth > 0){
-    s = mean(x)
-    node$s = s
-    node$add_left_child(Node$new())
-    node$add_right_child(Node$new())
-    create_regression_tree(node$get_left_child(), x[x<s], y[x<s], depth - 1)
-    create_regression_tree(node$get_right_child(), x[x>=s], y[x>=s], depth - 1)
-  }
-  else{
-    node$y = mean(y)
-  }
-}
-
-b <- Node$new()
-create_regression_tree(b, x, y, 3)
-b_regression_tree <- Tree$new("binary regression tree", b)
-b_regression_tree
-b
