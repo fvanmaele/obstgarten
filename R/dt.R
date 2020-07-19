c24431ad2bc181884d3edba2e151d65d29b6e62b
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2020-07-19 14:06:21 +0200

    remove cart.R
    (non-trivial) implementation of the abstract interface is only needed for external class. obstgarten::Baum and obstgarten::Gabel can provide it directly.

--- R/dt.R
+++ R/dt.R
@@ -1,3 +1,22 @@
+#' Check if a tree is a regression or a classification tree.
+#'
+#' This means:
+#' * The tree is a binary tree.
+#' * Every inner node (a node with 2 child nodes) has a split index (integer)
+#'   and a split point (numeric) attribute.
+#' * Every leaf has a value y as attribute. For a regression tree, y is numeric;
+#'   for a classification tree, y is an integer.
+#'
+#' @param Tree the tree to check. isCart() is generic and can be implemented for
+#'   any graph data structure (e.g. igraph).
+#' @param mode "regression" for a regression tree, "classification" for a
+#'   classification tree.
+#' @return TRUE if Tree is a regression or classification tree, FALSE otherwise.
+#' @seealso [isRegressionTree()], [isClassificationTree()]
+isCart <- function(Tree, ...) {
+  UseMethod("isCart")
+}
+
 #' Implementation of isCart() for data.tree::Node.
 #'
 #' @param Tree the tree to check.
@@ -25,9 +44,14 @@ isCart.Node <- function(Tree, mode) {
   }
 }
 
-#' Implementation of plotCart() for data.tree
-plotCart.Node <- function(Tree) {
-  # TODO
+#' Return a pruned subtree of a given tree,
+#'
+#' A pruned subtree is a subtree with the same root as the tree it is taken from.
+#' @param Tree the tree to prune.
+#' @param Node the first node of the branch to remove.
+#' @return A list containing the leaves of the pruned subtree.
+pruneBranch <- function(Tree, ...) {
+  UseMethod("pruneBranch")
 }
 
 #' Implementation of pruneBranch() for data.tree
@@ -36,15 +60,23 @@ plotCart.Node <- function(Tree) {
 #' @param Node the first node of the branch to remove.
 #' @return A list containing the leaves of the pruned subtree.
 pruneBranch.Node <- function(Tree, Node) {
-  # TODO
+  stop("function is not implemented")
+}
+
+#' Evaluate a decision rule
+#'
+#' The decision rule on a CART is given by:
+#' \deqn{f(x) = \sum_{m=1}{#T} y_m I_{A_{m}(x)}}
+#' where \eqn{x\in\prod([a_i,b_i])} and \eqn{A_m} is the partition induced by
+#' the CART.
+#'
+#' @param Tree the CART to evaluate.
+#' @param x a numeric vector representing a given data point x.
+#' @return
+evalCart <- function(Tree, ...) {
+  UseMethod("evalCart")
 }
 
 evalCart.Node <- function(Tree, x) {
-  #TODO
-}
-
-addSiblings.Node <- function(Tree, Child0, Child1) {
-  stopifnot(class(Child0) == "Node")
-  stopifnot(class(Child1) == "Node")
-  # TODO
+  stop("function is not implemented")
 }
