eb46072c8956d78e5703446ecb34f49e86362516
Ferdinand Vanmaele <fvanmaele@mailbox.org>
2020-07-25 00:52:01 +0200

    Remove memoise
    In testing on 2000 points of sin+noise data, memoise on subproblems made finding the optimal split point (on the root node) 7 times slower...
    
    Either something is broken or this is a dead end.

--- R/greedy.R
+++ R/greedy.R
@@ -1,23 +1,18 @@
-# c_sum <- function(y, n) {
-#   return(sum(y) / n)
-# }
-
-# mem_c_sum <- memoise::memoise(c_sum)
+which_is_min <- function(x) {
+  y <- seq_along(x)[x == min(x)]
+  if (length(y) > 1L) {
+    sample(y, 1L)
+  } else {
+    y
+  }
+}
 
 R_sum <- function(y1, y2, n) {
-  c1_hat <- sum(y1) / n # mem_c_sum(y1, n)
-  c2_hat <- sum(y2) / n # mem_c_sum(y2, n)
+  c1_hat <- sum(y1) / n
+  c2_hat <- sum(y2) / n
 
   return(sum((y1 - c1_hat)^2) + sum((y2 - c2_hat)^2))
 }
-
-# mem_R_sum <- memoise::memoise(R_sum)
-
-# R_part <- function(s, j, A) {
-#
-# }
-
-# mem_R_part <- memoise::memoise(R_sum)
 
 #' Risk factor R_n depending on split point and split index. (regression tree)
 #'
@@ -40,24 +35,25 @@ R_hat <- function(s, j, A, n) {
 #' partition of training data in an inner node v. (regression tree)
 #'
 #' Ties between found minima are broken at random.
-#' TODO: sample of dim for random forests
 #' @param A partition of training data (X_i, Y_i) (matrix)
 #' @param n size of the set of training data (integer)
 #' @param d dimension of the training data X_i1..X_id
 R_hat_min <- function(A, n, d) {
-  j_names <- sapply(1:d, function(i) paste0("j = ", i))
   # argmin: s_j \in (X_1j, .., X_nj) \sub A, j \in (1, .., d)
-  G <- array(dim=c(nrow(A), 2, d), dimnames=list(NULL, c("s", "R"), j_names))
+  G <- array(dim=c(nrow(A), 2, d))
+  dimnames(G) <- list( NULL, c("s", "R"), sapply(1:d, function(i) paste0("j = ", i)))
   min_s <- rep(NA, j)
 
   for (j in 1:d) {
     for (i in seq_along(A[, j])) {
       s <- A[i, j]
-      G[i, 1, j] <- s
-      G[i, 2, j] <- R_hat(s, j, A, n)
+      G[i, "s", j] <- s
+      G[i, "R", j] <- R_hat(s, j, A, n)
     }
+    min_s[[j]] <- G[which_is_min(G[, "R", j]), "s", j]
   }
   print(G)
+  print(min_s)
   stop() # todo: find minimal j, s
 
 }
@@ -65,7 +61,7 @@ R_hat_min <- function(A, n, d) {
 #' Create a regression tree greedily based on training data.
 #' @param XY matrix with columns $1..$d (representing the
 #' training data X_i) and column $y (for the corresponding values Y_i)
-#' @param steps The amount of iterations before halting the algorithm
+#' @param depth The amount of steps before halting the algorithm
 #' (defaults to 10)
 #' @param threshold
 #' @param mode
@@ -98,16 +94,16 @@ cart_greedy <- function(XY, depth = 10, threshold = 1, mode = "regression") {
         node$y <- NA
 
         # update attributes of left child
-        rows_lt <- which(node$points[, j] < s) # FIXME: memoise
+        rows_lt <- which(node$points[, j] < s) # FIXME: memoise?
         childL <- Gabel$new()
         childL$points <- node$points[rows_lt, , drop=FALSE]
-        childL$y <- sum(childL$points[, "y"]) / n # FIXME: memoise
+        childL$y <- sum(childL$points[, "y"]) / n # FIXME: memoise?
 
         # update attributes of right child
-        rows_gt <- setdiff(1:nrow(node$points), rows_lt) # FIXME: memoise
+        rows_gt <- setdiff(1:nrow(node$points), rows_lt) # FIXME: memoise?
         childR <- Gabel$new()
         childR$points <- node$points[rows_gt, , drop=FALSE]
-        childR$y <- sum(childR$points[, "y"]) / n # FIXME: memoise
+        childR$y <- sum(childR$points[, "y"]) / n # FIXME: memoise?
 
         # update tree and stack
         Cart$append(node$label, childL, childR)
