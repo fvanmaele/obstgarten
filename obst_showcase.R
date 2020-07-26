cd28de5ec794e63e8a99e4f5bb10e7947781f4f7
Ferdinand Vanmaele <fvanmaele@mailbox.org>
2020-07-26 21:46:02 +0200

    Improved plot for greedy algo
    Similar to Richter, Abb 6.15

--- obst_showcase.R
+++ obst_showcase.R
@@ -31,16 +31,27 @@ stopifnot(G2$childL$label == 4L) # 4L
 stopifnot(G2$childR$label == 5L) # 5L
 
 # ----
-M <- generate_sin_data(100, sigma=0.2)
+M <- generate_sin_data(150, sigma=0.2)
 dimnames(M) <- list(NULL, c(1, "y"))
-T2 <- cart_greedy(M, depth=3)
+T2 <- cart_greedy(M, depth=5)
 T2$validate()
 
 P <- T2$partition(T2$root, 1)
 x <- unname(P$part[, 1])
 x <- c(min(M[, 1]), x)
 y <- P$y
-ggplot() +
-  geom_step(data=data.frame(x = x, y = y), mapping=aes(x=x, y=y)) +
-  geom_point(data=data.frame(x= M[, 1], y=M[, 2]), mapping=aes(x=x, y=y))
 
+x <- c(x, max(M[, 1]))
+x1 <- numeric(0)
+x2 <- numeric(0)
+for (i in seq_along(x)) {
+  if (i == length(x)) break
+  x1 <- c(x1, x[[i]])
+  x2 <- c(x2, x[[i+1]])
+}
+
+ggplot() +
+  geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, colour = "segment"),
+               size=1, data=data.frame(x1 = x1, x2 = x2, y1 = y, y2 = y)) +
+  geom_point(data=data.frame(x= M[, 1], y=M[, 2]), mapping=aes(x=x, y=y), size=1) +
+  geom_function(data=data.frame(x = runif(200, min=0, max=1)), fun = function(x) sin(2 * pi * x))
