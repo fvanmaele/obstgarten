97995464ce42f5757940ab5f06c5b7a1b0b92473
Ferdinand Vanmaele <fvanmaele@mailbox.org>
2020-07-31 17:51:56 +0200

    implement greedy algo for classification trees
    Richter, Definition 6.16
    
    To avoid duplication of code, add an additional argument for the min function.

--- R/greedy.R
+++ R/greedy.R
@@ -1,4 +1,14 @@
-R_part <- function(s, j, A) {
+#' Title
+#'
+#' @param s
+#' @param j
+#' @param A
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cart_part <- function(s, j, A) {
   rows_lt <- A[, j] < s
   rows_gt <- !rows_lt
 
@@ -6,14 +16,19 @@ R_part <- function(s, j, A) {
               A2 = A[rows_gt, , drop=FALSE]))
 }
 
-R_hat <- function(y1, y2) {
-  c1_hat <- sum(y1) / length(y1)
-  c2_hat <- sum(y2) / length(y2)
-
-  return(sum((y1 - c1_hat)^2) + sum((y2 - c2_hat)^2))
-}
-
-R_grid <- function(A, d) {
+#' Title
+#'
+#' @param A
+#' @param d
+#' @param f
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cart_grid <- function(A, d, f) {
+  stopifnot(length(formals(f)) == 2)
   G_dn <- list(NULL, c("s", "R"), sapply(1:d, function(i) paste0("j = ", i)))
   G <- array(dim=c(nrow(A), 2, d), dimnames=G_dn)
 
@@ -21,10 +36,10 @@ R_grid <- function(A, d) {
   for (j in 1:d) {
     for (i in seq_along(A[, j])) {
       s <- A[i, j]
-      P <- R_part(s, j, A) # new partition A1, A2
+      P <- cart_part(s, j, A) # new partition A1, A2
 
       if (nrow(P$A1) > 0) {
-        R <- R_hat(P$A1[, "y"], P$A2[, "y"])
+        R <- f(P$A1[, "y"], P$A2[, "y"])
       } else {
         message("no data points for partition j = ", j, ", s = ", s)
         R <- NA_real_ # no data points in new partition, skip
@@ -36,16 +51,38 @@ R_grid <- function(A, d) {
   return(G)
 }
 
+R_hat <- function(y1, y2) {
+  c1_hat <- sum(y1) / length(y1)
+  c2_hat <- sum(y2) / length(y2)
+
+  return(sum((y1 - c1_hat)^2) + sum((y2 - c2_hat)^2))
+}
+
+C_hat <- function(y1, y2) {
+  c1_p <- length(y1[y1 == (sum(y1) / length(y1))]) / length(y1)
+  c2_p <- length(y2[y2 == (sum(y2) / length(y2))]) / length(y2)
+
+  # note that y1 may contain duplicate entries (if x_i, x_j -> y_i == y_j)
+  return(length(y1) * (1 - c1_p) + length(y2) * (1 - c2_p))
+}
+
 #' Find optimal split index \eqn{j} and split point \eqn{s} for a given
-#' partition A (regression tree)
+#' partition A (regression or classification tree)
 #'
 #' @param A subset of training data \eqn{(X_i, Y_i)} (matrix)
 #' @param d dimension of the training data \eqn{X_{i_1}..X_{i_d}}
+#' @param mode
 #'
 #' @return list containing optimal parameters \eqn{j}, \eqn{s}
 #' @export
-R_min <- function(A, d) {
-  G <- R_grid(A, d)
+R_min <- function(A, d, mode = "regression") {
+  if (mode == "regression") {
+    G <- cart_grid(A, d, R_hat)
+  } else if (mode == "classification") {
+    G <- cart_grid(A, d, C_hat)
+  } else {
+    stop("invalid mode: must be regression or classification")
+  }
   min_s <- apply(G, MARGIN=3, function(M) {
     idx <- which.min(M[, "R"]) # TODO: break ties at random
     M[idx, "s"]
@@ -67,6 +104,7 @@ R_min <- function(A, d) {
 #'   10)
 #' @param threshold (integer)
 #' @param sample (logical)
+#' @param mode
 #'
 #' @return
 #' @export
@@ -103,7 +141,7 @@ cart_greedy <- function(XY, depth = 10L, threshold = 1L, sample = FALSE) {
 
       if(nrow(node$points) > threshold) {
         # optimal subdivision
-        params <- R_min(node$points, d)
+        params <- R_min(node$points, d, mode = "regression")
         stopifnot(all(!is.na(params$j), !is.infinite(params$j)))
         stopifnot(all(!is.na(params$s), !is.infinite(params$s)))
 
