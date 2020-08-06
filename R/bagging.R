06495922181bb1bf79a044bec37c1648f54de78e
Viktor Spreng <m.spreng@stud.uni-heidelberg.de>
2020-08-06 17:58:23 +0200

    finished bagging implementation for regression

--- R/bagging.R
+++ R/bagging.R
@@ -1,18 +1,20 @@
-#' ATTENTION!! method predict YET TO BE IMPLEMENTED.
-#' ATTENTION!! method cart YET TO BE IMPLEMENTED.
+library(obstgarten)
+
+#' ATTENTION!! method not finished for classification case.
 #'
 #' @description
 #' Performs bagging on a dataset and returns predictions.
 #'
 #' @param B integer: number of bootstrap samples
-#' @param x_train data.frame: training data of dimensions Number of Samples x Features
-#' @param x_test data.frame: test data of dimensions Number of Samples x Features
+#' @param x_train data.frame: training data with labels of dimensions Number of Samples x Features + 1
+#' @param x_test data.frame: test data with labels of dimensions Number of Samples x Features + 1
 #' @param regression logical: specify whether tree is a classification or a
 #' regression tree. default=TRUE, TRUE for regression, FALSE for classification
 #'
 #' @return vector: of size Number of Samples containing bagged predictions to dataset
 #' character vector for classification case and double vector for regression case
 bagging <- function(B, x_train, x_test, regression=TRUE) {
+  dimnames(x_test) <- list(NULL, c(1, "y"))
   nb_samples <- dim(x_train)[1]
   nb_test_samples <- dim(x_test)[1]
   predictions <- matrix(rep(0., nb_test_samples * B), nrow=nb_test_samples, ncol=B)
@@ -22,15 +24,29 @@ bagging <- function(B, x_train, x_test, regression=TRUE) {
     return (names(which.max(table(vector))))
   }
 
+  if (regression) {
+
   for (i in 1:B) {
     # drawing bootstrap sample from training data with replacement
     x_b <- x_train[sample(1:nb_samples, size=nb_samples, replace=TRUE), ]
 
     # (over-)fitting tree to bootstrap sample via CART algorithm
-    trees[i] <- cart(x_b, regression=regression) # method yet to be implemented, return cart for x_b
+    dimnames(x_b) <- list(NULL, c(1, "y"))
+    trees[[i]] <- cart_greedy(x_b, depth=5, threshold=1) # return cart for x_b
+    trees[[i]]$validate()
 
-    # predicting with tree
-    predictions[, i] <- predict(data=x_test, tree=trees[i]) # method yet to be implemented for trees
+    predict <- function(x) {
+      return(cart_predict(x=x, node=trees[[i]]$root))
+    }
+
+    predictions[, i] <- apply(x_test[, 1, drop=FALSE], MARGIN=1, predict) # predicting with current tree
+
+  }
+
+  }
+
+  else {
+    stop("classification case not yet implemented!")
   }
 
   # returning predictions for test set via mean in regression case and via majority vote in classification case
@@ -38,4 +54,12 @@ bagging <- function(B, x_train, x_test, regression=TRUE) {
   else return(apply(predictions, MARGIN=1, majorityVote))
 
 }
+
+# M <- generate_sin_data(n, sigma=0.2)
+# dimnames(M) <- list(NULL, c(1, "y"))
+# y <- bagging(10, x_train = M, x_test = M)
+
+# plot(M[, 1], M[, 2])
+# plot(M[, 1], y)
+
 
