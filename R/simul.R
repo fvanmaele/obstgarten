b742a1976411ba5b28d5c3a05d95826522b4ead1
Max Viktor Spreng <max.spreng.17ucl.ac.uk>
2020-08-09 12:58:16 +0200

    script for functions needed to perform simulationsstudie

---
+++ R/simul.R
@@ -0,0 +1,36 @@
+library(obstgarten)
+library(tidyverse)
+
+bv_greedy <- function(depths_list, sigma=0.2, n=150, reps=400) {
+
+  predict <- function(x) {
+    return(cart_predict(x, node=tree$root))
+  }
+
+  params_list <- list()
+  ret <- list()
+  count <- 1
+
+  for (depth in depths_list) {
+    pred <- matrix(0., nrow=reps, ncol=n)
+
+    params_list[[count]] <- depth
+    grid <- seq(0, 1, len=n)
+
+    for (i in 1:reps) {
+      x <- generate_sin_data(n, grid=grid)
+      dimnames(x) <- list(NULL, c(1, "y"))
+      print(x)
+      tree <- cart_greedy(x, depth=depth)
+      pred[i, ] <- apply(x[, 1, drop=FALSE], MARGIN=1, predict) # predicting with current tree
+    }
+
+    ret[[count]] <- apply(pred, MARGIN=2, function(x) c(mean(x), sd(x)))
+    count <- count + 1
+  }
+  bv_data <- list(params_list, ret)
+  save("bv_data", file=str_c("data/simul/","bv_greedy_", format(Sys.time(), "%Y%m%d-%H%M%S")))
+}
+
+# bv_greedy(list(5L, 10L, 15L), n=150, reps=2)
+
