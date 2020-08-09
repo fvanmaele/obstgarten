5f5f35580714665c2dbb81ba0bad582e17a9e088
Max Viktor Spreng <max.spreng.17ucl.ac.uk>
2020-08-09 20:19:36 +0200

    added bv_plot method to plot bias variance plot for different CARTs

--- R/simul.R
+++ R/simul.R
@@ -1,6 +1,8 @@
 library(obstgarten)
 library(tidyverse)
+library(patchwork)
 
+# bias variance data for 400 reps for CARTs of different depths
 bv_greedy <- function(depths_list, sigma=0.2, n=150, reps=400) {
 
   predict <- function(x) {
@@ -34,3 +36,42 @@ bv_greedy <- function(depths_list, sigma=0.2, n=150, reps=400) {
 # generate test date for different depths values of the CART algorithm with 400 reps
 # and 150 data points
 # bv_greedy(list(5L, 10L, 15L), n=150, reps=400)
+
+#bias variance plot for CARTs for different depths
+bv_plot <- function(data) {
+
+  grid <- seq(0, 1, len=150)
+
+  depth_list <- data[[1]]
+  data <- data[[2]]
+  pd <- position_dodge(0.1)
+
+  plt_data <- data[[1]]
+  df1 <- data.frame(x=grid, mean=plt_data[1,], std=plt_data[2, ])
+  plt_data <- data[[2]]
+  df2 <- data.frame(x=grid, mean=plt_data[1,], std=plt_data[2, ])
+  plt_data <- data[[3]]
+  df3 <- data.frame(x=grid, mean=plt_data[1,], std=plt_data[2, ])
+
+  gg <- ggplot(df1, aes(x=grid, y=mean)) +
+    scale_colour_manual("",
+                        breaks = c("depth 5", "depth 10", "depth 15"),
+                        values = c("depth 5"="green", "depth 10"="red",
+                                   "depth 15"="blue")) +
+    geom_ribbon(aes(ymin = mean - std, ymax = mean + std), fill = "red", data=df3, alpha=0.75) +
+    geom_ribbon(aes(ymin = mean - std, ymax = mean + std), fill = "blue", data=df2, alpha=0.75) +
+    geom_ribbon(aes(ymin = mean - std, ymax = mean + std), fill = "green", alpha=0.75) +
+    geom_line(aes(x=grid, y=mean, colour="depth 15"), data=df3, alpha=1) +
+    geom_line(aes(x=grid, y=mean, colour="depth 10"), data=df2, alpha=1) +
+    geom_line(alpha=1, aes(colour="depth 5")) +
+    geom_line(aes(x=grid, y=sin(2*pi*grid))) +
+    ggtitle("Prediction of different CART generated Trees") +
+    ylab("y") +
+    xlab("x")
+
+  print(gg)
+
+}
+
+# load("data/simul/bv_greedy_20200809-132632")
+# bv_plot(bv_data)
