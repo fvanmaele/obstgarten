f53c115c6ba980234f900c2619d52057b1ba2738
Max Viktor Spreng <max.spreng.17ucl.ac.uk>
2020-08-10 15:14:47 +0200

    wrote rudimentary test for bagging method

--- tests/testthat/test-bagging.R
+++ tests/testthat/test-bagging.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("bagging: argument assertions", {
+  x <- generate_sin_data(10)
+  expect_error(bagging(x_train=x, x_test=x, B=12))
+  expect_error(bagging(x, B="f"))
+  expect_error(bagging(x_train=x, x_test=x, B=10L, regression=2))
+  expect_error(bagging(x_train=x, x_test=x, B=10L, regression="hey"))
+})
+
+test_that("bagging: argument x_train and x_test", {
+  expect_error(bagging(x_train=2, x_test=2, B=10L))
+  expect_error(bagging(x_train="A", x_test="A", B=10L))
+  expect_error(bagging(x_train=generate_sin_data(10), x_test=10), B=10L)
+  expect_error(bagging(x_train=matrix(c(1,1,2,2,3,3), 2, 3)),
+               x_test=matrix(c(1,1,2,2,3,3), 2, 3), B=10L)
+
+  x <- generate_sin_data(10)
+  B <- sample(1:10, 1)
+  res <- bagging(B=B, x_train=x, x_test=x)
+  expect_type(res, "double")
 })
