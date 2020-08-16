adab4dfca1973638aeb97709d8a7bf32b77e0309
Ferdinand Vanmaele <fvanmaele@mailbox.org>
2020-08-16 18:25:47 +0200

    move cart_predict to greedy.R

--- R/greedy.R
+++ R/greedy.R
@@ -207,3 +207,25 @@ cart_greedy <- function(XY, depth = 10L, mode="regression", threshold = 1L, samp
   return(Cart)
 }
 
+#' Title
+#'
+#' @param x
+#' @param node
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cart_predict = function(x, node) { # list or vector
+  # check if vector x matches tree dimension
+  stopifnot(length(x) == (ncol(node$points)-1))
+
+  if (is.null(node$childR) && is.null(node$childL)) {
+    # leaf node found -> return value
+    return(node$y)
+  } else if (x[[node$j]] < node$s) {
+    return(cart_predict(x, node$childL))
+  } else {
+    return(cart_predict(x, node$childR))
+  }
+}
