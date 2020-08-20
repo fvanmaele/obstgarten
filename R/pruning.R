6e07e3f4a49630c78563a0fa54eef606f2049212
Loquebantur <Falk.Loewner@gmail.com>
2020-08-20 17:25:46 +0200

    lamba equal 0 allowed

--- R/pruning.R
+++ R/pruning.R
@@ -134,7 +134,7 @@ cart_greedy_prune <-
         q_threshold = q_threshold,
         q_pct = q_pct
       )
-    stopifnot("lambda is not numeric and greater than 0"= is.numeric(lambda) & lambda > 0L)
+    stopifnot("lambda is not numeric and greateror equal than 0"= is.numeric(lambda) & lambda >= 0L)
 
     # internal helper for generating a mask of leafes
     mLeafes <- function(tree, mask) {
